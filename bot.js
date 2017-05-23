db4c367fb0a9aa0c8af359eb64b40b4de0c2c29b
Renemari Padillo <padillo.renemari@gmail.com>
2017-05-24 05:14:46 +0800

    [MISC] Updated code comments & removed whitespaces

--- bot.js
+++ bot.js
@@ -26,6 +26,8 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it needs to execute a command
+    // for this script it will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
