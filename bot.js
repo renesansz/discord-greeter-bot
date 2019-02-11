9ebea431a1b501121f378084a6c7b26781113e48
Camilla Greer <camilla.garner.greer@gmail.com>
2019-02-11 12:11:00 -0500

    ! commands to create, add, and echo back lists

--- bot.js
+++ bot.js
@@ -25,6 +25,8 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+let title = ''
+let entries = []
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it needs to execute a command
@@ -41,11 +43,32 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             // !ping
             case 'ping':
                 bot.sendMessage({ to: channelID, message: 'Pong!' });
-	    // !new [Title]
+		break;
+	    // !new Title of List
 	    case 'new':
-		let title = message.substring(5);
+		title = message.substring(5);
 	        bot.sendMessage({ to: channelID, message: 'Starting new list: '+title });
-            break;
+		break;
+	    // !title
+	    // Repeat the current title
+	    case 'title':
+	        bot.sendMessage({ to: channelID, message: 'Current title is: '+title });
+		break;
+	    case 'add':
+		entry = message.substring(5);
+		entries.push(entry);
+	        bot.sendMessage({ to: channelID, message: 'Adding entry'+entries.length+'to '+title });
+		break;
+	    case 'end':
+		let listmessage = '';
+		listmessage = listmessage + title + "\n"
+		for (var entrynum = 0; entrynum < entries.length; entrynum++) {
+		    let humnum = entrynum + 1;
+	            listmessage = listmessage + humnum+". "+entries[entrynum]+"\n";
+		}
+		listmessage = listmessage + "-----END-----"
+	        bot.sendMessage({ to: channelID, message: listmessage });
+		break;
             default:
                 bot.sendMessage({ to: channelID, message: 'Unknown command.' });
         }
