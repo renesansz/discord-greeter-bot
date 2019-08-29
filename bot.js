ccdc66486a9101c83d505b1d23d5adf8a1eb9865
Rene Padillo <developer@renesansz.me>
2019-08-29 08:23:00 +0800

    [MISC] Prettified code

--- bot.js
+++ bot.js
@@ -1,15 +1,23 @@
+// Import libraries
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 
+
+// Event listener when a user connected to the server.
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Event listener when a user sends a message in the chat.
 client.on('message', msg => {
+
+  // We check the message content and looks for the word "ping", so we can have the bot respond "pong"
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
+
 });
 
+// Initialize bot by connecting to the server
 client.login(auth.token);
