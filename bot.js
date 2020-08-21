8b18c2d332c3df862a02a8d7209fd264afb5c49e
Summit <sarkarsum7@yahoo.com>
2020-08-20 20:48:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,15 +31,22 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'Bibot':
                 bot.sendMessage({ to: channelID, message: 'Morbleu!' });
             break;
+            case 'PercySmart':
+                bot.sendMessage({ to: channelID, message: 'He was calmly eating his soup, laughing with pleasant good-humour, as if he had come all the way to Calais for the express purpose of enjoying supper at this filthy inn, in the company of his arch-enemy.' });
+            break;
+            case 'MoneyNoMatter':
+                bot.sendMessage({ to: channelID, message: 'Those friends who knew, laughed to scorn the idea that Marguerite St. Just had married a fool for the sake of the worldly advantages with which he might endow her. They knew, as a matter of fact, that Marguerite St. Just cared nothing about money, and still less about a title.' });
+            break;
+            case 'Brains':
+                bot.sendMessage({ to: channelID, message: 'Money and titles may be hereditary,” she would say, “but brains are not.' });
+            break;
+            case 'SPpoem':
+                bot.sendMessage({ to: channelID, message: 'We seek him here, we seek him there, Those Frenchies seek him everywhere. Is he in heaven?—Is he in hell? That demmed, elusive Pimpernel?' });
+            break;
             case 'Haters':
                 bot.sendMessage({ to: channelID, message: 'How that stupid, dull Englishman ever came to be admitted within the intellectual circle which revolved round “the cleverest woman in Europe,” as her friends unanimously called her, no one ventured to guess—a golden key is said to open every door, asserted the more malignantly inclined.' });
             break;
-	    case 'UselessJunk':
-                bot.sendMessage({ to: channelID, message: 'How dare you call me useless junk! I will hunt you down and blah blah blah blah blah blah blah blah!' });
-            break;
-	    case 'help':
-                bot.sendMessage({ to: channelID, message: 'The only command that I have right now is ?Bibot, which yields the result \"Morbleu!\"' });
-            break;
+
             default:
                 bot.sendMessage({ to: channelID, message: 'ce que le ...? I don\'t know that command.' });
         }
